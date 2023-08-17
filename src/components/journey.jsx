2be33d60d975ea1c5bac47b8185bc9a237c0c7f3
gourain <ngoutsoprainsong@gmail.com>
2023-08-17 16:23:27 +0100

    responsive on xl and md removed

--- src/components/journey.jsx
+++ src/components/journey.jsx
@@ -11,8 +11,8 @@ import acesy from '../assets/acesy.png'
 const Journey = () => {
   return (
     <>
-        <section className='lg:-mt-24 mt-24 overflow-hidden xl:text-4xl'>
-            <h1 className='lg:text-8xl text-3xl font-bold xl:text-9xl' >Journey
+        <section className='lg:-mt-24 mt-24 overflow-hidden'>
+            <h1 className='lg:text-8xl text-3xl font-bold' >Journey
                 <div class="absolute z-0 bg-gradient-to-tr from-primary/25 to-dark/10 lg:w-[1120px] w-[200px] h-[400px] blur-3xl -mt-40">
                 </div>
             </h1>
@@ -25,46 +25,46 @@ const Journey = () => {
                             className='w-[75px] h-[100px]'
                             alt="" 
                         />
-                        <div className='flex ml-3 h-auto items-center text-2xl font-black xl:text-4xl '>
+                        <div className='flex ml-3 h-auto items-center text-2xl font-black'>
                             University of <br />Yaounde 1
                         </div>
                     </div>
 
-                    <div className='flex lg:w-1/2 w-full lg:ml-auto mx-auto lg:justify-end justify-center text-black lg:-space-x-12 -space-x-2 lg:mb-0 mb-4'>
-                        <div className='group w-1/4 rounded-xl text-center space-y-4 boxes-journey bg-primary opacity-20'>
-                            <div className='w-full flex justify-center py-3 pb-2'>
+                    <div className='flex lg:w-1/2 w-full lg:ml-auto mx-auto lg:justify-end justify-center text-black lg:-space-x-12 space-x-0 lg:mb-0 mb-4'>
+                        <div className='group w-1/4 rounded-xl text-center space-y-2 boxes-journey bg-primary opacity-20'>
+                            <div className='w-full flex justify-center pt-2 pb-0'>
                                 <img 
-                                className='h-20 w-20 object-contain'
+                                className='lg:h-20 h-12 w-20 object-contain'
                                 src={Uy1_black} alt="" />
                             </div>
                             <p className='w-full text-black text-center pb-2 font-bold'>
                                 University
                             </p>
                         </div>
-                        <div className='group w-1/4 bg-slate-200 rounded-xl text-center space-y-4 boxes-journey'>
-                            <div className='w-full flex justify-center py-3 pb-2'>
+                        <div className='group w-1/4 bg-slate-200 rounded-xl text-center space-y-2 boxes-journey'>
+                            <div className='w-full flex justify-center pt-2 pb-0'>
                                 <img 
-                                className='h-20 w-20 object-contain'
+                                className='lg:h-20 h-12 w-20 object-contain'
                                 src={yak} alt="" />
                             </div>
                             <p className='w-full text-black text-center pb-2 font-bold'>
                                 YAKNEMA
                             </p>
                         </div>
-                        <div className='group w-1/4 bg-slate-200 rounded-xl text-center space-y-4 boxes-journey'>
-                            <div className='w-full flex justify-center py-3 pb-2'>
+                        <div className='group w-1/4 bg-slate-200 rounded-xl text-center space-y-2 boxes-journey'>
+                            <div className='w-full flex justify-center pt-2 pb-0'>
                                 <img 
-                                className='h-20 w-20 object-contain'
+                                className='lg:h-20 h-12 w-20 object-contain'
                                 src={lohce} alt="" />
                             </div>
                             <p className='w-full text-black text-center pb-2 font-bold'>
                                 LOHCE
                             </p>
                         </div>
-                        <div className='group w-1/4 bg-slate-200 rounded-xl text-center space-y-4 boxes-journey'>
-                            <div className='w-full flex justify-center py-3 pb-2'>
+                        <div className='group w-1/4 bg-slate-200 rounded-xl text-center space-y-2 boxes-journey'>
+                            <div className='w-full flex justify-center pt-2 pb-0'>
                                 <img 
-                                className='h-20 w-20 object-contain'
+                                className='lg:h-20 h-12 w-20 object-contain'
                                 src={acesy} alt="" />
                             </div>
                             <p className='w-full text-black text-center pb-2 font-bold'>
@@ -75,32 +75,32 @@ const Journey = () => {
                 </div>
                 <div className='flex flex-wrap justify-between mt-16'>
                     <div className='lg:w-1/2 w-full text-center font-semibold text-xl'>
-                        <div className='flex my-2 xl:my-6'>
+                        <div className='flex my-2'>
                             <img src={location} alt="" className='h-6' />
-                            <div className='ml-3 xl:text-3xl'>
+                            <div className='ml-3'>
                                 Yaoundé, Cameroon
                             </div>
                         </div>
-                        <div className='flex my-2 xl:my-6'>
-                            <img src={note2} alt="" className='h-6' />
-                            <div className='ml-3 xl:text-3xl'>
+                        <div className='flex my-2'>
+                            <img src={watch} alt="" className='h-6' />
+                            <div className='ml-3'>
                                 05 Years
                             </div>
                         </div>
-                        <div className='flex my-2 xl:my-6'>
-                            <img src={watch} alt="" className='h-6' />
-                            <div className='ml-3 xl:text-3xl'>
+                        <div className='flex my-2'>
+                            <img src={note2} alt="" className='h-6' />
+                            <div className='ml-3'>
                             Master Computer Science
                             </div>
                         </div>
                         <div className='flex bg-primary/20 backdrop-blur-2xl w-fit lg:mt-20 mb-6 p-3 rounded-xl cursor-pointer transition-all hover:scale-95 duration-200 ease-in-out'>
                             <img src={tour} alt="" className='h-6' />
-                            <div className='ml-3 xl:text-3xl'>
+                            <div className='ml-3'>
                                 Take tour
                             </div>
                         </div>
                     </div>
-                    <div className='lg:w-1/2 lg:text-xl xl:text-3xl pb-6 text-justify xl:pl-9'>
+                    <div className='lg:w-1/2 lg:text-xl pb-6 text-justify'>
                         The Software Enginee Master's program at the University of Yaounde I in Cameroon is a great option for students who want to pursue a career in software enginee. The program is well-respected and offers a rigorous curriculum that covers all aspects of software enginee, from design and development to testing and deployment.
                         Concerning me it continue to be a great experience full all nkowledge and discovery
                     </div>
--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -15,16 +15,16 @@ const Welcome = () => {
             <div className='flex lg:flex-row flex-row-reverse justify-between h-auto lg:h-100 mt-6'>
                 <div className='lg:mt-36 mt-4 lg:w-2/5 w-full h-full z-50'>
                     <div className='lg:absolute z-50 h-1/2 '>
-                        <p className='lg:text-2xl xl:text-5xl text-3xl font-bold'>
+                        <p className='lg:text-2xl text-3xl font-bold'>
                             Hey, ✌🏽 I am...️    
                         </p><br />
-                        <p className='lg:text-[9.2rem] xl:text-[16rem] sm:text-[2rem] text-[4.2rem] ml-4 lg:-mt-16 font-extrabold'>
+                        <p className='md:text-[9.2rem] sm:text-[2rem] text-[4.2rem] ml-4 lg:-mt-16 font-extrabold'>
                             Rainsong
                         </p><br />
-                        <p className='lg:text-2xl xl:text-5xl text-xl lg:font-normal font-bold lg:-mt-12 mt-2 mb-12'>
+                        <p className='lg:text-2xl text-xl lg:font-normal font-bold lg:-mt-12 mt-2 mb-12'>
                             {'{{'} I mean, Rainsong NGOUTSOP.{'}}'}
                         </p>
-                        <p className='lg:text-2xl xl:text-5xl text-xl lg:mt-4 mt-24 xl:leading-snug lg:leading-normal leading-relaxed w-full'>
+                        <p className='lg:text-2xl text-xl lg:mt-4 mt-24 lg:leading-normal leading-relaxed w-full'>
                             Laughing in the Face of Browser <br /> Compatibility – The {' '}
                             <span className='text-primary font-bold'>Front-End</span> <br />
                              Developer's Battle Cry! 😄
@@ -35,7 +35,7 @@ const Welcome = () => {
                     <div className='lg:ml-4 w-full shadow-2xl shadow-primary rounded-full overflow-hidden lg:border-[15px] border-[5px] border-primary '>
                         <img src={me} alt="my picture" className='w-full shadow-xl' />
                     </div>
-                    <div className='lg:absolute lg:block hidden xl:bottom-[32rem]  bottom-72 rounded-l-3xl -right-44 shadow-2xl shadow-slate-700 border-l-2 border-y-2 border-primary overflow-hidden  xl:w-92 xl:h-52  w-46 h-52 object-cover ring'>
+                    <div className='lg:absolute lg:block hidden bottom-72 rounded-l-3xl -right-44 shadow-2xl shadow-slate-700 border-l-2 border-y-2 border-primary overflow-hidden  w-46 h-52 object-cover ring'>
                             <img src={a1} alt="" className='object-cover' />
                             <div className='bg-primary text-xl flex justify-center items-center h-1/5 w-1/3 absolute bottom-0 rounded-tr-xl' >
                                 avendre
@@ -44,7 +44,7 @@ const Welcome = () => {
                 </div>
             </div>
 
-            <div className='absolute -bottom-24 left-1 xl:left-48 lg:left-48 arrow'>
+            <div className='absolute -bottom-24 left-1 lg:left-48 arrow'>
                 <div className='flex justify-center pt-3'>
                     <img src={mouse} alt="" className='w-3/4 drop-shadow-xl drop-shadow-white' /> 
                 </div>
